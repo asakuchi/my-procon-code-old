c82b521bdd910be488ad9b2ba1d3885d07cdb1e2
paleApricot <49332371+paleApricot-it@users.noreply.github.com>
2019-04-28 18:40:55 +0900

    ABC-D AC

--- AtCoderBeginnerContest/ABC125/D.py
+++ AtCoderBeginnerContest/ABC125/D.py
@@ -1,2 +1,20 @@
-N, A, B, C = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
 
+minus_count = 0
+
+for a in A:
+    if a < 0:
+        minus_count += 1
+
+total = 0
+min_num = 10 ** 9
+
+for a in A:
+    total += abs(a)
+    min_num = min(min_num, abs(a))
+
+if minus_count % 2 == 1:
+    total -= 2 * min_num
+
+print(total)
