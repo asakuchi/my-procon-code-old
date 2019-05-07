da4920f1b6d69bfcb12abc8e250e1fccee3f4190
paleApricot <49332371+paleApricot-it@users.noreply.github.com>
2019-05-08 01:05:17 +0900

    add ABC110-C

--- AtCoderBeginnerContest/ABC110/C.py
+++ AtCoderBeginnerContest/ABC110/C.py
@@ -1,2 +1,28 @@
-N, A, B, C = map(int, input().split())
+S = input()
+T = input()
 
+char_map = {}
+char_map_t = {}
+
+result = 'Yes'
+
+for s, t in zip(S, T):
+    if s in char_map:
+        if t == char_map[s]:
+            continue
+        else:
+            result = 'No'
+            break
+    else:
+        char_map[s] = t
+
+    if t in char_map_t:
+        if s == char_map_t[t]:
+            continue
+        else:
+            result = 'No'
+            break
+    else:
+        char_map_t[t] = s
+
+print(result)
