373e64ab4f018327fb3e5da410cc7569a056d64e
paleApricot <49332371+paleApricot-it@users.noreply.github.com>
2019-05-30 00:07:07 +0900

    mod ABC093-B

--- AtCoderBeginnerContest/ABC093/B.py
+++ AtCoderBeginnerContest/ABC093/B.py
@@ -2,10 +2,10 @@ A, B, K = map(int, input().split())
 
 result = set()
 
-for n in range(A, A + K):
+for n in range(A, min(A + K, B + 1)):
     result.add(n)
 
-for n in range(B - K + 1, B + 1):
+for n in range(max(A, B - K + 1), B + 1):
     result.add(n)
 
 for n in sorted(list(result)):
