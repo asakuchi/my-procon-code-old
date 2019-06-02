dd30baa54ca60d6272be7c2d37817b3a2ec6a585
paleApricot <49332371+paleApricot-it@users.noreply.github.com>
2019-06-02 23:46:32 +0900

    mod ABC091-B

--- AtCoderBeginnerContest/ABC091/B.py
+++ AtCoderBeginnerContest/ABC091/B.py
@@ -1,2 +1,23 @@
-N, A, B, C = map(int, input().split())
+N = int(input())
 
+words = {'na': 0}  # 選択しない
+
+for i in range(N):
+
+    s = input()
+
+    if s in words:
+        words[s] += 1
+    else:
+        words[s] = 1
+
+M = int(input())
+
+for i in range(M):
+
+    t = input()
+
+    if t in words:
+        words[t] -= 1
+
+print(max(words.values()))
